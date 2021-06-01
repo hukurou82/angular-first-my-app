ed6233186e410821f4aab0b365f7bb4ca92aa12a
JunyaMasuto <hukurou82@gmail.com>
2021-06-01 23:04:45 +0900

    products.tsに商品情報を。nfForで回して製品情報に飛べるように。

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { products } from '../../products';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,7 +8,13 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent implements OnInit {
+  product;
+
   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.product = products[+params.get('productId')];
+    });
+  }
 }
